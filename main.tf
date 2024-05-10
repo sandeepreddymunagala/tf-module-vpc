100a6c83a6dbcf108b9848cf14141669edaad332
sandeep <munagalasandeepreddy@gamail.com>
2024-05-10 14:58:15 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_support = true
+
+  tags = merge({
+    Name = "${var.env}-vpc"
+  }, var.tags)
+}
+
+
+
+resource "aws_subnet" "main" {
+  count = length(var.web_subnet_cider_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = merge({
+    Name = "${var.env}-web_subnet"
+  }, var.tags)
 }
