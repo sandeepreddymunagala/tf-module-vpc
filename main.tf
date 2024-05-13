b9921a970e9d74ee80a5c18a9f32620149badcbb
sandeep <munagalasandeepreddy@gamail.com>
2024-05-13 19:10:55 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -68,8 +68,8 @@ resource "aws_route" "peer-route" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
-/*resource "aws_route" "default-vpc-apeer-route" {
+resource "aws_route" "default-vpc-apeer-route" {
   route_table_id            = var.default_vpc_rt
   destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}*/
+}
