ccfa36263e1c9c300da342cdc6ac68b48bab98cd
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 17:04:25 +0200

    learn terraform

---
+++ subnets/main.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "main" {
+  vpc_id = var.vpc_id
+  count =length(var.cidr_block)
+  cidr_block = element(var.cidr_block, count.index)
+  availability_zone = element(var.az, count.index )
+
+  tags = merge({
+    Name = "${var.env}-${var.subnet_name}-subnet"
+  },
+    var.tags)
+}
+
--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
 variable "cidr_block" {}
-/*variable "env" {}
+variable "env" {}
 variable "tags" {}
 variable "subnets" {}
 variable "az" {
   default = ["us-east-1a","us-east-1b"]
 }
+/*/
 variable "default_vpc_id" {}
 variable "default_vpc_rt" {}*/
