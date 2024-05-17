75aef0201d27fd24611b86e3aaeb254d0085589e
sandeep <munagalasandeepreddy@gamail.com>
2024-05-17 16:19:16 +0200

    learn terraform

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "cidr_block" {}
-variable "env" {}
+/*variable "env" {}
 variable "tags" {}
 variable "subnets" {}
 variable "az" {
   default = ["us-east-1a","us-east-1b"]
 }
 variable "default_vpc_id" {}
-variable "default_vpc_rt" {}
+variable "default_vpc_rt" {}*/
